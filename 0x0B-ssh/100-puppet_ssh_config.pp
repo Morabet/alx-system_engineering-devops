dbcd1aa628258fbfb640c71213a3d36467c5c365
Morabet <ismailelmorabet2@gmail.com>
2023-12-25 03:09:39 -0800

    100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,13 @@
+# sets up a client SSH configuration file so that we can connect to a server without using a password
 
 file { 'passwd auth':
   ensure => present,
-  path   => '/etc/ssh/ssh_config',
+  path   => '~/.ssh/school',
   line   => '    PasswordAuthentication no',
 }
 
 file { 'identity file':
   ensure => present,
-  path   => '/etc/ssh/ssh_config',
+  path   => '~/.ssh/school',
   line   => '     IdentityFile ~/.ssh/school',
 }
